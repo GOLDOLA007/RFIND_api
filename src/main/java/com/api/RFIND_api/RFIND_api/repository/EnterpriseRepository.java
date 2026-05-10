9e4ac9f2d9f722d930659e4b65fce56a079c8051
Kaua <kauaoliveira18062007@gmail.com>
2026-05-09 22:48:49 -0300

    feat: create EnterpriseRepository

---
+++ src/main/java/com/api/RFIND_api/RFIND_api/repository/EnterpriseRepository.java
@@ -0,0 +1,9 @@
+package com.api.RFIND_api.RFIND_api.repository;
+
+
+import com.api.RFIND_api.RFIND_api.model.Enterprise;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnterpriseRepository extends JpaRepository<Enterprise, Long> {
+    
+}
