e972bafb892df303243d4cebcd16aca3bc22ba20
Kaua <kauaoliveira18062007@gmail.com>
2026-05-09 22:20:18 -0300

    chore: create project

---
+++ src/main/java/com/api/RFIND_api/RFIND_api/RfindApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.RFIND_api.RFIND_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfindApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfindApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/RFIND_api/RFIND_api/RfindApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.RFIND_api.RFIND_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfindApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
