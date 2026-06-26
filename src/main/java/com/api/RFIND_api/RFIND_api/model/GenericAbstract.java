0804ea39f4bd5caeabf819afcea325a1b6fc5ee5
Kaua <kauaoliveira18062007@gmail.com>
2026-06-26 16:33:25 -0300

    feat: add Generic Abstract class

---
+++ src/main/java/com/api/RFIND_api/RFIND_api/model/GenericAbstract.java
@@ -0,0 +1,7 @@
+package com.api.RFIND_api.RFIND_api.Model;
+
+import com.api.RFIND_api.RFIND_api.Repository.GenericInterface;
+
+public abstract class GenericAbstract<E, ID> implements GenericInterface<E, ID> {
+
+}
