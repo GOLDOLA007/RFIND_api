6e86451e268c49bafc52e2c5e8d3b930446ce9dc
Kaua <kauaoliveira18062007@gmail.com>
2026-06-26 16:28:08 -0300

    refactor: remove files

--- src/main/java/com/api/RFIND_api/RFIND_api/repository/EnterpriseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.RFIND_api.RFIND_api.repository;
-
-
-import com.api.RFIND_api.RFIND_api.model.Enterprise;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnterpriseRepository extends JpaRepository<Enterprise, Long> {
-
-}
