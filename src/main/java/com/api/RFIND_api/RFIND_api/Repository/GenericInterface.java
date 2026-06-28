f5bb973302086c7780e60e0591b29dc95f23ce67
kaua <kauaoliveira18062007@gmail.com>
2026-06-27 21:40:38 -0300

    refactor: remove unecessary files

--- src/main/java/com/api/RFIND_api/RFIND_api/Repository/GenericInterface.java
+++
@@ -1,5 +0,0 @@
-package com.api.RFIND_api.RFIND_api.Repository;
-
-public interface GenericInterface<E, ID> {
-    public E insert(E enterprise);
-}
--- src/main/java/com/api/RFIND_api/RFIND_api/model/GenericAbstract.java
+++
@@ -1,7 +0,0 @@
-package com.api.RFIND_api.RFIND_api.Model;
-
-import com.api.RFIND_api.RFIND_api.Repository.GenericInterface;
-
-public abstract class GenericAbstract<E, ID> implements GenericInterface<E, ID> {
-
-}
