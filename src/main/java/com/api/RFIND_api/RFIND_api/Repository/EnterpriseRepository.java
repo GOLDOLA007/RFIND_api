5922638c6e6b8ae67eada802ba245ccd7af3b672
kaua <kauaoliveira18062007@gmail.com>
2026-06-27 22:17:30 -0300

    fix: add Entity and primary key in the JpaRepository parameters

--- src/main/java/com/api/RFIND_api/RFIND_api/Repository/EnterpriseRepository.java
+++ src/main/java/com/api/RFIND_api/RFIND_api/Repository/EnterpriseRepository.java
@@ -1,6 +1,8 @@
 package com.api.RFIND_api.RFIND_api.Repository;
 
+import com.api.RFIND_api.RFIND_api.model.Enterprise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EnterpriseRepository extends JpaRepository {
+public interface EnterpriseRepository extends JpaRepository<Enterprise, Long> {
+
 }
