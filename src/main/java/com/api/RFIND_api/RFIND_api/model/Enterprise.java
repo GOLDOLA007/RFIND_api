a7eef8e73f0c5bcb579a5880709204497fa2e45a
kaua <kauaoliveira18062007@gmail.com>
2026-06-27 22:02:18 -0300

    feat: add JPA annotions and id, cnpj, name, email and password fields

--- src/main/java/com/api/RFIND_api/RFIND_api/model/Enterprise.java
+++ src/main/java/com/api/RFIND_api/RFIND_api/model/Enterprise.java
@@ -1,5 +1,29 @@
-package com.api.RFIND_api.RFIND_api.Model;
+package com.api.RFIND_api.RFIND_api.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "enterprise")
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
 public class Enterprise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
+    @Column(nullable = false, unique = true, length = 14)
+    private int cnpj;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 6)
+    private String password;
 }
