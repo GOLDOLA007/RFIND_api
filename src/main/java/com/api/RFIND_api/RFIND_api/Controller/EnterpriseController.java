517bf2ca28d1aa48d4aeacd81251887ec918f4f1
kaua <kauaoliveira18062007@gmail.com>
2026-06-29 11:45:37 -0300

    feat: create EnterpriseController and add listall and create endpoints

--- src/main/java/com/api/RFIND_api/RFIND_api/Controller/EnterpriseController.java
+++ src/main/java/com/api/RFIND_api/RFIND_api/Controller/EnterpriseController.java
@@ -1,4 +1,31 @@
 package com.api.RFIND_api.RFIND_api.Controller;
 
+import com.api.RFIND_api.RFIND_api.DTO.Request.EnterpriseRequest;
+import com.api.RFIND_api.RFIND_api.DTO.Response.EnterpriseResponse;
+import com.api.RFIND_api.RFIND_api.Service.EnterpriseService;
+import com.api.RFIND_api.RFIND_api.model.Enterprise;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/enterprise")
+@RequiredArgsConstructor
 public class EnterpriseController {
+
+    private final EnterpriseService enterpriseService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@Valid @RequestBody EnterpriseRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(enterpriseService.create(request));
+    }
+
+    @GetMapping
+    public List<EnterpriseResponse> listAll(){
+        return enterpriseService.listAll();
+    }
 }
