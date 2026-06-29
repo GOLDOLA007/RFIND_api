dccd5910e81b162759481191e8187e0cfe552b2b
kaua <kauaoliveira18062007@gmail.com>
2026-06-29 16:50:07 -0300

    refactor: change findByEmail to existsByEmail

--- src/main/java/com/api/RFIND_api/RFIND_api/Repository/EnterpriseRepository.java
+++ src/main/java/com/api/RFIND_api/RFIND_api/Repository/EnterpriseRepository.java
@@ -6,5 +6,5 @@ import com.api.RFIND_api.RFIND_api.model.Enterprise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EnterpriseRepository extends JpaRepository<Enterprise, Long> {
-    boolean findByEmail(String email);
+    boolean existsByEmail(String email);
 }
